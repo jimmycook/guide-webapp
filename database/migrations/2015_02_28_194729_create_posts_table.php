e4666f29f5b178745e7604542be82cc42f6916e7
Jimmy Cook <jimmy.cook.1993@gmail.com>
2015-02-28 20:09:45 +0000

    added initial files

---
+++ database/migrations/2015_02_28_194729_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->string('title');
+			$table->string('slug')->unique();
+			$table->boolean('draft');
+			$table->text('body');
+			$table->boolean('image');
+			$table->boolean('featured');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
